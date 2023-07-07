dec555b9eb7592b214c3c85eae31a466ca01669f
Dalton Dayton <daltondayton1@gmail.com>
2023-07-07 13:22:55 -0600

    button

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,3 +1,4 @@
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -11,9 +12,9 @@ const AddUser = (props) => {
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
-        <label htmlFor="age">Age</label>
+        <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
